8a2d82c4532c716e03ad638d31556035fc6828c9
Stephen Newman <steve@newman.digital>
2021-03-11 15:07:25 +0000

    Add parallel tests and run summary sample.

--- linq-comparison/SamplesBenchmarks.cs
+++ linq-comparison/SamplesBenchmarks.cs
@@ -2,6 +2,8 @@
 using Bogus;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace linq_comparison
 {
@@ -52,7 +54,7 @@ namespace linq_comparison
 
 
         [Benchmark]
-        public int GetCountOfAlcoholicDrinksUsingFor()
+        public int GetCountOfAlcoholicDrinksUsingForAndCountProperty()
         {
             var countOfAlcoholicDrinks = 0;
             for (var index = 0; index < this.drinks.Count; index++)
@@ -62,6 +64,99 @@ namespace linq_comparison
                     countOfAlcoholicDrinks++;
                 }
             }
+
+            return countOfAlcoholicDrinks;
+        }
+
+
+        [Benchmark]
+        public int GetCountOfAlcoholicDrinksUsingForAndCountMethod()
+        {
+            var countOfAlcoholicDrinks = 0;
+            for (var index = 0; index < this.drinks.Count(); index++)
+            {
+                if (this.drinks[index].IsAlcoholic)
+                {
+                    countOfAlcoholicDrinks++;
+                }
+            }
+
+            return countOfAlcoholicDrinks;
+        }
+
+        [Benchmark]
+        public int GetCountOfAlcoholicDrinksUsingParallelWhereCount()
+        {
+            return this.drinks.AsParallel().Where(d => d.IsAlcoholic).Count();
+        }
+
+        [Benchmark]
+        public int GetCountOfAlcoholicDrinksUsingParallelCount()
+        {
+            return this.drinks.AsParallel().Count(d => d.IsAlcoholic);
+        }
+
+        [Benchmark]
+        public int GetCountOfAlcoholicDrinksUsingParallelForEach()
+        {
+            var countOfAlcoholicDrinks = 0;
+            Parallel.ForEach(
+                this.drinks,
+                () => 0,
+                (drink, loopState, localSum) =>
+                {
+                    if (drink.IsAlcoholic)
+                    {
+                        localSum++;
+                    }
+                    return localSum;
+                },
+                (localSum) => Interlocked.Add(ref countOfAlcoholicDrinks, localSum)
+            );
+
+            return countOfAlcoholicDrinks;
+        }
+
+        [Benchmark]
+        public int GetCountOfAlcoholicDrinksUsingParallelForAndCountProperty()
+        {
+            var countOfAlcoholicDrinks = 0;
+            Parallel.For(
+                0,
+                this.drinks.Count - 1,
+                () => 0,
+                (index, loopState, localSum) =>
+                {
+                    if (this.drinks[index].IsAlcoholic)
+                    {
+                        localSum++;
+                    }
+                    return localSum;
+                },
+                (localSum) => Interlocked.Add(ref countOfAlcoholicDrinks, localSum)
+            );
+
+            return countOfAlcoholicDrinks;
+        }
+
+        [Benchmark]
+        public int GetCountOfAlcoholicDrinksUsingParallelForAndCountMethod()
+        {
+            var countOfAlcoholicDrinks = 0;
+            Parallel.For(
+                0,
+                this.drinks.Count() - 1,
+                () => 0,
+                (index, loopState, localSum) =>
+                {
+                    if (this.drinks[index].IsAlcoholic)
+                    {
+                        localSum++;
+                    }
+                    return localSum;
+                },
+                (localSum) => Interlocked.Add(ref countOfAlcoholicDrinks, localSum)
+            );
 
             return countOfAlcoholicDrinks;
         }
